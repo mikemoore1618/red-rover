e07b6db574dc50ae4dc8130eb11df4f18bd2197e
Mike Moore <mikemoore1618@gmail.com>
2018-07-02 10:29:15 -0700

    User update controller

--- controllers/users.js
+++ controllers/users.js
@@ -16,4 +16,27 @@ show: (req, res) => {
         res.json({ success: true, message: "User found", User: showUser })
       })
     },
+
+     // create a new User
+     create: (req, res) => {
+        User.create(req.body, (err, savedUser) => {
+         if(err) throw err;
+         res.json({ success: true, message: "User created.", User: savedUser })
+           })
+       },
+//update a User(toggle)
+    //for patch request we dont need to supply request body because we want app to know what we want to change it to-true or false(toggle)
+
+    update: (req,res) => {
+        let id = req.params.id
+        User.findById(id, (err, UserItem) => {
+          if(err) throw err;
+          UserItem.completed = !UserItem.completed
+          UserItem.save((err, savedUser) => {
+            if(err) throw err;
+            res.json({ success: true, message: "User Updated", User: savedUser})
+          })
+        })
+    },
+     
 }
