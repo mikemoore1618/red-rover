e744ec2ca546c0cf91d11ce122dcfb8ce355c482
Mike Moore <mikemoore1618@gmail.com>
2018-07-02 12:00:52 -0700

    _by: req.user

---
+++ controllers/sites.js
@@ -0,0 +1,50 @@
+const Site = require('../models/Site.js')
+
+module.exports = {
+
+// SHOW ALL SITESS
+    index: (req, res) => {
+      Site.find({}, (err, allSites) => {
+        if(err) throw err;
+        res.json(allSites)
+      })  
+    },
+
+// SHOW SITES
+show: (req, res) => {
+    let id = req.params.id
+      Site.findById(id, (err, showSite) => {
+        if(err) throw err;
+        res.json({ success: true, message: " SITE FOUND", Site: showSite })
+      })
+    },
+
+// CREATE SITES
+    create: (req, res) => {
+        Site.create({ ...req.body, _by: req.user }, (err, savedSite) => {
+         if(err) throw err;
+         res.json({ success: true, message: "SITE CREATED", Site: savedSite })
+           })
+       },
+
+// EDIT SITES
+    update: (req,res) => {
+        let id = req.params.id
+        Site.findById(id, (err, updateSite) => {
+          if(err) throw err;
+          updateSite.save((err, savedSite) => {
+            if(err) throw err;
+            res.json({ success: true, message: "SITE UPDATED", Site: savedSite})
+          })
+        })
+    },
+     
+// DELETE SITES
+    destroy: (req, res) => {
+        let id = req.params.id
+        Site.findByIdAndRemove(id, (err, deletedSite) => {
+          if(err) throw err;
+          res.json({ success: true, message: "SITE DELETED" })
+        })
+    }
+}
