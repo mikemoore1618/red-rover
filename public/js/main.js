45c57bef057796ba6ab6a8db9ebe2b8f2d0bcd59
Mike Moore <mikemoore1618@gmail.com>
2018-07-02 16:17:05 -0700

    navbar functionality

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,7 @@ let $menuToggle = $('#menu-toggle')
 let $mainNavigation = $('#main-navigation')
 
     $menuToggle.on('click', () => {
+      console.log('clicked')
       $mainNavigation.toggleClass('open')
       $menuToggle.toggleClass('active')
     })
