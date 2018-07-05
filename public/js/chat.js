439ef7b596cc97ea28a0c1eec22c1713e432e3ca
Jack Cambou <jcambou@gmail.com>
2018-07-05 11:13:47 -0700

    Changed Chat so that current user is the one chatting, rather than input field for name

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,16 @@
 console.log("Chatbox JS Loaded")
 var socket = io()
-var $userTag = $('#usertag')
 var $message = $('#message')
 var $sendChat = $('#send-chat')
 var $chatList = $('#chat-list')
 
 $sendChat.on("click", () => {
-    var data = {usertag: $userTag.val(), message: $message.val()}
+    var data = { message: $message.val()}
     socket.emit("sendmessage", data)
     $message.val("")
     $message.focus()
 })
 
 socket.on('receivemessage', (data) => {
-    $chatList.prepend(`<li> ${data.usertag} said the following: ${data.message}  </li>`)
+    $chatList.prepend(`<li> ${currentUser.name} said the following: ${data.message}  </li>`)
 })
